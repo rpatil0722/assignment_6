2e0f9d243ff23506d9fcb997f8ab86cc34913b81
rpatil0722 <90164861+rpatil0722@users.noreply.github.com>
2021-10-10 21:43:51 -0700

    assignment_6

---
+++ 6/odd-or-even.js
@@ -0,0 +1,10 @@
+/*eslint-env browser*/
+
+var i;
+for (i = 0; i <= 15; i += 1) {
+    if (i % 2 === 0) {
+        window.console.log(i + " is an even.");
+    } else {
+        window.console.log(i + " is an odd.");
+    }
+}
